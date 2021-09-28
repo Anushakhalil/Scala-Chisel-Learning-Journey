5329568d46502ec081095f536e7f12852690d765
Anushakhalil <sweet.anosha5@gmail.com>
2021-09-28 15:53:53 +0500

    lab7

--- src/main/scala/lab7/Ex_1.scala
+++ src/main/scala/lab7/Ex_1.scala
@@ -2,18 +2,42 @@ package lab7
 import chisel3._
 import chisel3.util._
 
-class Queue extends Module {
+class Ex_1 extends Module {           // priority arbiter
     val io = IO ( new Bundle {
         val in = Flipped(Decoupled(UInt(8.W))) 
+        val in2 = Flipped(Decoupled(UInt(8.W)))
         val consumer = Decoupled(UInt(8.W ))
         })
 
-    val producer_q1 = Queue(io.in,8)
-    val producer_q2 = Queue(io.in,8)
 
+    val producer_q1 = Queue(io.in)
+    val producer_q2 = Queue(io.in2)
     val arb = Module(new Arbiter(UInt(), 2))
-    arb.io.in(0) <> producer_q1.deq()
-    arb.io.in(1) <> producer_q2.deq()
+
+    arb.io.in(0) <> producer_q2
+    arb.io.in(1) <> producer_q1
 
     io.consumer <> arb.io.out
 }
+
+// class Ex_1 extends Module {           // RR arbiter
+//     val io = IO ( new Bundle {
+//         val in = Flipped(Decoupled(UInt(8.W))) 
+//         val in2 = Flipped(Decoupled(UInt(8.W)))
+//         val in3 = Flipped(Decoupled(UInt(8.W)))
+//         val consumer = Decoupled(UInt(8.W ))
+//         })
+
+
+//     val producer_q1 = Queue(io.in)
+//     val producer_q2 = Queue(io.in2)
+//     val producer_q3 = Queue(io.in3)
+//     val arb = Module(new RRArbiter(UInt(), 3))
+
+//     arb.io.in(0) <> producer_q1
+//     arb.io.in(1) <> producer_q2
+//     arb.io.in(2) <> producer_q3
+
+//     io.consumer <> arb.io.out
+// }
+
--- src/test/scala/lab7/Ex_1Test.scala
+++ src/test/scala/lab7/Ex_1Test.scala
@@ -1,10 +1,21 @@
 package lab7
 import chisel3._
-import chisel3.iotesters._
+import org.scalatest._ 
+import chiseltest._
 
-class Ex_1Test(c : Ex_1) extends PeekPokeTester(c) {
-    poke(c.io.in , 2.U)
-    step(1)
-    expect(c.io.consumer,4.U)
+class Ex_1Test extends FreeSpec with ChiselScalatestTester{
+    "Ex_1 Test" in{
+        test(new Ex_1){ c => 
+        c.io.in.bits.poke(1.U)
+        c.io.in2.bits.poke(2.U)
+        // c.io.in3.bits.poke(3.U)
+        c.io.in.valid.poke(1.B)
+        c.io.in2.valid.poke(1.B)
+        // c.io.in3.valid.poke(1.B)
+        c.io.consumer.ready.poke(1.B)
+        c.clock.step(5)
+        c.io.consumer.bits.expect(2.U)
 
+        }
+    }
 }
