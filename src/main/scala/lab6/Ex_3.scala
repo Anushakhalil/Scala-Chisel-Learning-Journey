59f336db9b476c93741e29c8301a2f1b53eabbdb
Anushakhalil <sweet.anosha5@gmail.com>
2021-09-29 23:59:45 +0500

    one shot timer updated

--- src/main/scala/lab6/Ex_3.scala
+++ src/main/scala/lab6/Ex_3.scala
@@ -11,22 +11,22 @@ class Ex_3 extends Module{
     val next = WireInit(0.U)
     val done = timer_count =/= 0.U 
 
-    // io.out := 0.B
-    // when(io.reload){
-    //    next := din
+    io.out := 0.B
+    when(io.reload){
+       next := din
        
-    //    when(done){
-    //         next := timer_count - 1.U
-    //         timer_count := timer_count - 1.U
-    //         io.out := 0.B
+       when(done){
+            next := timer_count - 1.U
+            timer_count := timer_count - 1.U
+            io.out := 0.B
 
-    //     }.otherwise{
-    //         timer_count := next
-    //         io.out := 1.B
-    //     }
+        }.otherwise{
+            timer_count := next
+            io.out := 1.B
+        }
 
-    // }.otherwise{
-    //     DontCare
-    // }
+    }.otherwise{
+        DontCare
+    }
 
 }
