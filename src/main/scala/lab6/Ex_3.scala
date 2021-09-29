d2fa6b942ec468a91f0a84e4865bc9ad1d8ed643
Anushakhalil <sweet.anosha5@gmail.com>
2021-09-29 17:07:23 +0500

    labc completed

--- src/main/scala/lab6/Ex_3.scala
+++ src/main/scala/lab6/Ex_3.scala
@@ -1,21 +1,25 @@
-// package lab6
-// import chisel3._
+package lab6
+import chisel3._
 
-// class Ex_3(val din: UInt = 1.U) extends Module{
-//     val io = IO(new Bundle{
-//         val reload = Input(Bool())
-//         val timer_count = RegInit(0.U(8.W))
-//         val done = timer_count === 0.U
-//         val next = WireInit(0.U)
-//     })
+class Ex_3 extends Module{
+    val io = IO (new Bundle{
+        val reload = Input(Bool())
+        val out = Output(Bool())
+    })
 
-//     when(io.reload){
-//     io.next := din
+    val timer_count = RegInit(0.U(8.W))
+    val next = WireInit(6.U)
+    
+    io.out := 0.U
 
-//     }.elsewhen(!io.done ) {
-//     io.next := io.timer_count - 1.U
-
-//     }.otherwise{
-//         io.timer_count := io.next
-//     }
-// }
+    when(io.reload){
+        when(timer_count =/= 0.U){
+            next := timer_count - 1.U
+        }.otherwise{
+            io.out := 1.B
+            timer_count := next
+        }
+    }otherwise{
+        DontCare
+    }
+}
--- src/test/scala/lab6/Ex_3Test.scala
+++ src/test/scala/lab6/Ex_3Test.scala
@@ -1,15 +1,16 @@
-// package lab6
-// import chisel3._ 
-// import chiseltest._ 
-// import org.scalatest._ 
+package lab6
+import chisel3._ 
+import chiseltest._ 
+import org.scalatest._ 
 
-// class Ex_3Test extends FreeSpec with ChiselScalatestTester{
-//     "two shot counter" in {
-//         test( new Ex_3()){ c =>
-//             c.io.reload(1.B)
-//             c.clock.step(10)
-//         }
+class Ex_3Test extends FreeSpec with ChiselScalatestTester{
+    "two shot counter" in {
+        test(new Ex_3){ c =>
+            // c.io.din.poke(2.U)
+            c.io.reload.poke(1.B)
+            c.clock.step(2)
+        }
 
-//     }
+    }
 
-// }
+}
